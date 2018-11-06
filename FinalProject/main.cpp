1068655fa57edfbeb7316f1f13e235d7323ce748
japneetsingh <japneet2@illinois.edu>
2018-11-06 16:46:43 -0600

    initial commit

---
+++ FinalProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FinalProject
+//
+//  Created by Japneet Singh on 11/6/18.
+//  Copyright © 2018 Japneet Singh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
